3c0fabfe3ed73371c84aa3cf03af0bc1493eb44c
LIN Linsen <z1x2c3v441@gmail.com>
2020-05-23 17:45:55 +0800

    map

--- src/screens/RecordScreen.js
+++ src/screens/RecordScreen.js
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { View, FlatList,Text,ScrollView} from "react-native";
 import Record from "../compenents/Record";
-
+import Map from "../compenents/MAP";
 const RecordScreen = ({navigation}) => {
   return (
    
       <View style={{height:1000}}>
       <Record 
             navigation={navigation}/>
+      <Map  navigation={navigation}/>
        </View>
           
   );
